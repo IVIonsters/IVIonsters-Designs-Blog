d15fe1a3d172fe0ca0e96cd2fc603133c80eff8b
IVIonsters <zacharypolof@gmail.com>
2024-03-24 10:02:05 -0500

    added pseudo code comments to form and logic JavaScript files to explain what we want to create

---
+++ assets/js/form.js
@@ -0,0 +1,20 @@
+// collect user input for username, title , content:
+
+// collect submit button information as variable:
+
+// create issue if no information is entered - should link this html for unanswered questions -- ALERT
+
+//pull data from local storage, if no data, create empty array
+
+// event listener for submit button
+    // prevent default action? 
+
+// check if any field was left blank , or has error. If so, alert user to fill in all fields
+
+// create object with user input
+
+// push object to array
+
+// save array to local storage
+
+// clear form fields, if all data is correct proceed to blog page.
---
+++ assets/js/logic.js
@@ -0,0 +1,10 @@
+// Define variablwes for light-Dark mode toggle
+
+
+// Check for saved 'darkMode' in localStorage
+
+// Event to change the theme based on user preference
+
+//  if light mode, change to dark mode
+
+//  if dark mode, change to light mode
